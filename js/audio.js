6ec23862bb8855a8d6073cf75b4b4b93f8124dfa
manoajk <manoajk@yahoo.com>
2018-04-14 03:15:55 -0400

    Chart using D3 and window.AudioContext

---
+++ js/audio.js
@@ -0,0 +1,58 @@
+tempo = 100;
+
+var notes = [
+    [659, 4],
+    [659, 4],
+    [659, 4],
+    [523, 8],
+    [0, 16],
+    [783, 16],
+    [659, 4],
+    [523, 8],
+    [0, 16],
+    [783, 16],
+    [659, 4],
+    [0, 4],
+    [987, 4],
+    [987, 4],
+    [987, 4],
+    [1046, 8],
+    [0, 16],
+    [783, 16],
+    [622, 4],
+    [523, 8],
+    [0, 16],
+    [783, 16],
+    [659, 4]
+];
+
+// notes.reverse(); 
+tempo = 100;
+
+// create web audio api context
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+function playMelody(){
+    if (notes.length > 0){
+        note = notes.shift()
+        playNote(note[0],1000*256/(note[1]*tempo));
+    }
+}
+
+function playNote(frequency, duration) {
+// create Oscillator node
+var oscillator = audioCtx.createOscillator();
+
+oscillator.type = 'square';
+oscillator.frequency.value = frequency; // value in hertz
+oscillator.connect(audioCtx.destination);
+oscillator.start();
+  
+setTimeout(
+    function(){
+        oscillator.stop();
+        playMelody();
+    }, duration);
+}
+
+playMelody();
