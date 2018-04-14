7172e623f8cb24c7e5b50dfb9f39184d62e43315
manoajk <manoajk@yahoo.com>
2018-04-14 12:12:53 -0400

    Made audio call more modular

--- js/d3Interactions.js
+++ js/d3Interactions.js
@@ -1,51 +1,3 @@
-var myData = "date	New York	San Francisco	Austin\n\
-20111001	63.4	62.7	72.2\n\
-20111002	58.0	59.9	67.7\n\
-20111003	53.3	59.1	69.4\n\
-20111004	55.7	58.8	68.0\n\
-20111005	64.2	58.7	72.4\n\
-20111006	58.8	57.0	77.0\n\
-20111007	57.9	56.7	82.3\n\
-20111008	61.8	56.8	78.9\n\
-20111009	69.3	56.7	68.8\n\
-20111010	71.2	60.1	68.7\n\
-20111011	68.7	61.1	70.3\n\
-20111012	61.8	61.5	75.3\n\
-20111013	63.0	64.3	76.6\n\
-20111014	66.9	67.1	66.6\n\
-20111015	61.7	64.6	68.0\n\
-20111016	61.8	61.6	70.6\n\
-20111017	62.8	61.1	71.1\n\
-20111018	60.8	59.2	70.0\n\
-20111019	62.1	58.9	61.6\n\
-20111020	65.1	57.2	57.4\n\
-20111021	55.6	56.4	64.3\n\
-20111022	54.4	60.7	72.4\n";
-
-//     var myData = "date\tNew York\n\
-// 20111001\t63.4\n\
-// 20111002\t58.0\n\
-// 20111003\t53.3\n\
-// 20111004\t55.7\n\
-// 20111005\t64.2\n\
-// 20111006\t58.8\n\
-// 20111007\t57.\n\
-// 20111008\t61.8\n\
-// 20111009\t69.3\n\
-// 20111010\t71.2\n\
-// 20111011\t68.7\n\
-// 20111012\t61.8\n\
-// 20111013\t63.0\n\
-// 20111014\t66.9\n\
-// 20111015\t61.7\n\
-// 20111016\t61.8\n\
-// 20111017\t62.8\n\
-// 20111018\t60.8\n\
-// 20111019\t62.1\n\
-// 20111020\t65.1\n\
-// 20111021\t55.6\n\
-// 20111022\t54.4\n";
-
 var margin = {
     top: 20,
     right: 80,
@@ -83,8 +35,6 @@ var svg = d3.select("body").append("svg")
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var data = d3.tsv.parse(myData);
 
 color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
 
@@ -255,25 +205,7 @@ mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
         d3.select(this).select('text')
             .text(y.invert(pos.y).toFixed(2));
 
-        //notes.push([Math.trunc(y.invert(pos.y).toFixed(2) * 10), 4])
-        //playMelody()
-        if (notePos > 9) {
-            notePos = 0;
-        }
-        if (octPos > 4) {
-            octPos = 0;
-        }
-        Synth.play(instruments[0], noteTypes[notePos],oct[octPos],2);
-        counter++
-        if (counter == 100) {
-            notePos++;
-        }
-        if (counter == 25) {
-            octPos++;
-        }
-        console.log(notePos);
-        console.log("DAGFD" + "dgafd   "+octPos);
-        //Synth.play('piano', 'C#', 4, 2);
+        playNote(Math.trunc(y.invert(pos.y).toFixed(2)));
         
         return "translate(" + mouse[0] + "," + pos.y +")";
       });
